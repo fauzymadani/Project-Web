e46c782b431310478dcbeaf6d7da0d6417f54f54
fauzymadani <keperluansekolahfauzy@gmail.com>
2025-02-05 14:31:35 +0700

    fix span in changelog

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
-            <span>Copyright  Sisfo Anggota created by Abdulloh Faqih </span>
+            <span>Copyright  LibSys created by <a href="https://github.com/faqihboyy">Abdullah Faqih</a> and <a href="https://github.com/fauzymadani">Fauzy Madani</a> </span>
         </div>
     </div>
 </footer>
