7dd1be062a9ee18c30dd6ae3d03ec549b79da6d7
fauzymadani <keperluansekolahfauzy@gmail.com>
2025-02-12 19:26:12 +0700

    test pdf viewer

--- routes/web.php
+++ routes/web.php
@@ -20,23 +20,38 @@ use App\Http\Controllers\KategoriController;
 |
 */
 
-Route::get('/', function () {
-    return view('dashboard');
-})->name('dashboard');
+Route::get("/", function () {
+    return view("dashboard");
+})->name("dashboard");
 
-Route::resource('buku', BukuController::class)->middleware('iniLogin');
-Route::resource('anggota', AnggotaController::class)->middleware('iniLogin');
-Route::resource('dashboard', SessionController::class)->middleware('iniLogin');
-Route::resource('roles', RoleController::class)->middleware('iniLogin');
-Route::resource('peminjaman', PeminjamanController::class)->middleware('iniLogin');
-Route::resource('kategori', KategoriController::class)->middleware('iniLogin');
-Route::get('/anggota/create', [AnggotaController::class, 'create'])->name('anggota.create');
+Route::resource("buku", BukuController::class)->middleware("iniLogin");
+Route::resource("anggota", AnggotaController::class)->middleware("iniLogin");
+Route::resource("dashboard", SessionController::class)->middleware("iniLogin");
+Route::resource("roles", RoleController::class)->middleware("iniLogin");
+Route::resource("peminjaman", PeminjamanController::class)->middleware(
+    "iniLogin"
+);
+Route::resource("kategori", KategoriController::class)->middleware("iniLogin");
+Route::get("/anggota/create", [AnggotaController::class, "create"])->name(
+    "anggota.create"
+);
 
+Route::get("/login", [SessionController::class, "index"])->middleware(
+    "iniTamu"
+);
+Route::get("sesi", [SessionController::class, "index"])->middleware("iniTamu");
+Route::post("/sesi/login", [SessionController::class, "login"])->middleware(
+    "iniTamu"
+);
+Route::get("/sesi/logout", [SessionController::class, "logout"]);
 
-Route::get('/login', [SessionController::class, 'index'])->middleware('iniTamu');
-Route::get('sesi', [SessionController::class, 'index'])->middleware('iniTamu');
-Route::post('/sesi/login', [SessionController::class, 'login'])->middleware('iniTamu');
-Route::get('/sesi/logout', [SessionController::class, 'logout']);
+Route::get("/commits", [GithubController::class, "index"])->name(
+    "commits.index"
+);
+Route::get("/commits/{sha}", [GithubController::class, "show"])->name(
+    "commits.show"
+);
 
-Route::get('/commits', [GithubController::class, 'index'])->name('commits.index');
-Route::get('/commits/{sha}', [GithubController::class, 'show'])->name('commits.show');
+Route::get("/baca", function () {
+    return view("baca");
+});
